6434f82ca5881e719722977f5bf32e11105c64e4
Nicholas Brown <165951815+iTryKetchup@users.noreply.github.com>
2026-01-11 03:45:57 -0700

    Add files via upload

---
+++ StudentRegistration.java
@@ -0,0 +1,12 @@
+package courseProjectPhase1;
+
+public class StudentRegistration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Heello World");
+		System.out.println();
+		System.out.println("Congratulation on your first Java program");
+	}
+
+}
